9c38a4a28e49ab7db572a26553ce127dcd7fc692
mayagbarnes <maya8617@gmail.com>
2021-10-20 17:31:31 -0700

    Add elements

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Elements with Short Text:")
+st.title("st.title - This is a `test`")
+st.header("st.header - This is a `test`")
+st.subheader("st.subheader - This is a `test`")
+st.markdown("st.markdown - This is a `test`")
+st.write("st.write - This is a `test`")
+st.caption("st.caption - This is a `test`")
+
+
+st.title("Elements with Long Text:")
+st.title("st.title - Lorem ipsum `test` dolor sit amet, consectetur adipiscing elit. Aenean `test` non quam ut risus volutpat hendrerit `test` sit amet a arcu. Aenean at tempus urna, sed convallis metus. Phasellus `test` blandit urna vitae venenatis ultrices. `test`")
+st.header("st.header - Lorem ipsum `test` dolor sit amet, consectetur adipiscing elit. Aenean `test` non quam ut risus volutpat hendrerit `test` sit amet a arcu. Aenean at tempus urna, sed convallis metus. Phasellus `test` blandit urna vitae venenatis ultrices. `test`")
+st.subheader("st.subheader - Lorem ipsum `test` dolor sit amet, consectetur adipiscing elit. Aenean `test` non quam ut risus volutpat hendrerit `test` sit amet a arcu. Aenean at tempus urna, sed convallis metus. Phasellus `test` blandit urna vitae venenatis ultrices. `test`")
+st.markdown("st.markdown - Lorem ipsum `test` dolor sit amet, consectetur adipiscing elit. Aenean `test` non quam ut risus volutpat hendrerit `test` sit amet a arcu. Aenean at tempus urna, sed convallis metus. Phasellus `test` blandit urna vitae venenatis ultrices. `test`")
+st.write("st.write - Lorem ipsum `test` dolor sit amet, consectetur adipiscing elit. Aenean `test` non quam ut risus volutpat hendrerit `test` sit amet a arcu. Aenean at tempus urna, sed convallis metus. Phasellus `test` blandit urna vitae venenatis ultrices. `test`")
+st.caption("st.caption - Lorem ipsum `test` dolor sit amet, consectetur adipiscing elit. Aenean `test` non quam ut risus volutpat hendrerit `test` sit amet a arcu. Aenean at tempus urna, sed convallis metus. Phasellus `test` blandit urna vitae venenatis ultrices. `test`")
